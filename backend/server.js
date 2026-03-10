c9075e143af4d6acff10b8065f9ccdab3bb6be61
haruakinahidlose <haruakinahidlose@gmail.com>
2026-03-10 10:55:03 +0900

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import http from "http";
+import { WebSocketServer } from "ws";
+import routes from "./routes.js";
+import setupWS from "./ws.js";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/api", routes);
+
+const server = http.createServer(app);
+const wss = new WebSocketServer({ server });
+
+setupWS(wss);
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log("NebulaShift backend running on " + PORT));
