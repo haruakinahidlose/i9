65083f41d6c0fa7baa87b43db5f5c5721f916c5a
haruakinahidlose <haruakinahidlose@gmail.com>
2026-03-11 11:07:25 +0900

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,44 +1,25 @@
-import sqlite3 from "sqlite3";
-import { readFileSync } from "fs";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+import pg from "pg";
+const { Pool } = pg;
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
-// Correct paths for Railway
-const dbPath = __dirname + "/database.db";
-const schemaPath = __dirname + "/schema.sql";
-
-const db = new sqlite3.Database(dbPath);
-
-const schema = readFileSync(schemaPath, "utf8");
-db.exec(schema);
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
+});
 
 export default {
   run(query, params = []) {
-    return new Promise((resolve, reject) => {
-      db.run(query, params, function (err) {
-        if (err) reject(err);
-        else resolve(this);
-      });
-    });
+    return pool.query(query, params);
   },
 
   get(query, params = []) {
-    return new Promise((resolve, reject) => {
-      db.get(query, params, (err, row) => {
-        if (err) reject(err);
-        else resolve(row);
-      });
-    });
+    return pool
+      .query(query, params)
+      .then(res => res.rows[0] || null);
   },
 
   all(query, params = []) {
-    return new Promise((resolve, reject) => {
-      db.all(query, params, (err, rows) => {
-        if (err) reject(err);
-        else resolve(rows);
-      });
-    });
+    return pool
+      .query(query, params)
+      .then(res => res.rows);
   }
 };
