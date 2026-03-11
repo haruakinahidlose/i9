31688692500e5da34f807cee0e008a973b5a2529
haruakinahidlose <haruakinahidlose@gmail.com>
2026-03-11 19:16:32 +0900

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,21 +1,7 @@
-import pg from "pg";
-const { Pool } = pg;
+import pkg from "pg";
+const { Pool } = pkg;
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
+export const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
 });
-
-export default {
-  run(query, params = []) {
-    return pool.query(query, params);
-  },
-
-  get(query, params = []) {
-    return pool.query(query, params).then(res => res.rows[0] || null);
-  },
-
-  all(query, params = []) {
-    return pool.query(query, params).then(res => res.rows);
-  }
-};
