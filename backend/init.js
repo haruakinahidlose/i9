22370babd58e34fdf2fda90140a2868d097587da
haruakinahidlose <haruakinahidlose@gmail.com>
2026-03-11 20:00:39 +0900

    Update init.js

--- backend/init.js
+++ backend/init.js
@@ -1,10 +1,16 @@
 import { pool } from "./db.js";
 import fs from "fs";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
-const schema = fs.readFileSync("./backend/schema.sql", "utf8");
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// Correct absolute path
+const schemaPath = __dirname + "/schema.sql";
 
 (async () => {
     try {
+        const schema = fs.readFileSync(schemaPath, "utf8");
         await pool.query(schema);
         console.log("Database initialized");
     } catch (err) {
