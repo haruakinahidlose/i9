cdcf938d1772fcd1ac729adbd0b44a7bb7167d1b
haruakinahidlose <haruakinahidlose@gmail.com>
2026-03-12 08:18:23 +0900

    Update rooms.js

--- docs/js/rooms.js
+++ docs/js/rooms.js
@@ -1,41 +1,22 @@
-const API_BASE = "https://i9.up.railway.app/api";
-
-export async function loadRooms() {
+async function loadRooms() {
   const token = localStorage.getItem("token");
 
-  const res = await fetch(`${API_BASE}/rooms/list`, {
-    headers: { Authorization: token }
+  const res = await fetch("https://i9.up.railway.app/api/rooms/list", {
+    headers: { Authorization: `Bearer ${token}` }
   });
 
-  return await res.json();
+  const data = await res.json();
+  const box = document.getElementById("roomList");
+  box.innerHTML = "";
+
+  data.rooms.forEach(r => {
+    const div = document.createElement("div");
+    div.textContent = "#" + r.name;
+    div.onclick = () => openRoom(r.id);
+    box.appendChild(div);
+  });
 }
 
-export async function createRoom(name) {
-  const token = localStorage.getItem("token");
-
-  const res = await fetch(`${API_BASE}/rooms/create`, {
-    method: "POST",
-    headers: {
-      Authorization: token,
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({ name })
-  });
-
-  return await res.json();
-}
-
-export async function sendRoomMessage(room_id, content) {
-  const token = localStorage.getItem("token");
-
-  const res = await fetch(`${API_BASE}/rooms/send`, {
-    method: "POST",
-    headers: {
-      Authorization: token,
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({ room_id, content })
-  });
-
-  return await res.json();
+function openRoom(id) {
+  ws.send(JSON.stringify({ type: "join_room", id }));
 }
