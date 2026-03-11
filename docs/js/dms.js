10e4cf26e88243781789b33107080a244b1c26e8
haruakinahidlose <haruakinahidlose@gmail.com>
2026-03-12 08:18:37 +0900

    Update dms.js

--- docs/js/dms.js
+++ docs/js/dms.js
@@ -1,41 +1,22 @@
-const API_BASE = "https://i9.up.railway.app/api";
-
-export async function loadDMs() {
+async function loadDMs() {
   const token = localStorage.getItem("token");
 
-  const res = await fetch(`${API_BASE}/dms/list`, {
-    headers: { Authorization: token }
+  const res = await fetch("https://i9.up.railway.app/api/friends/list", {
+    headers: { Authorization: `Bearer ${token}` }
   });
 
-  return await res.json();
+  const data = await res.json();
+  const box = document.getElementById("dmList");
+  box.innerHTML = "";
+
+  data.friends.forEach(f => {
+    const div = document.createElement("div");
+    div.textContent = f.username;
+    div.onclick = () => openDM(f.friend_id);
+    box.appendChild(div);
+  });
 }
 
-export async function startDM(user_id) {
-  const token = localStorage.getItem("token");
-
-  const res = await fetch(`${API_BASE}/dms/start`, {
-    method: "POST",
-    headers: {
-      Authorization: token,
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({ user_id })
-  });
-
-  return await res.json();
-}
-
-export async function sendDM(dm_id, content) {
-  const token = localStorage.getItem("token");
-
-  const res = await fetch(`${API_BASE}/dms/send`, {
-    method: "POST",
-    headers: {
-      Authorization: token,
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({ dm_id, content })
-  });
-
-  return await res.json();
+function openDM(id) {
+  ws.send(JSON.stringify({ type: "join_dm", id }));
 }
