f426559d199f2ca05ea36b0c40498b3e47126bc2
haruakinahidlose <haruakinahidlose@gmail.com>
2026-03-12 08:17:40 +0900

    Update app.js

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,25 +1,11 @@
-import { connectWS } from "./ws.js";
-import { loadRooms } from "./rooms.js";
-import { loadFriends } from "./friends.js";
-import { loadDMs } from "./dms.js";
+window.onload = () => {
+  const token = localStorage.getItem("token");
+  if (!token) location.href = "index.html";
 
-const token = localStorage.getItem("token");
-if (!token) window.location.href = "index.html";
-
-const socket = connectWS(handleWS);
-
-async function init() {
-  const rooms = await loadRooms();
-  const friends = await loadFriends();
-  const dms = await loadDMs();
-
-  console.log("Rooms:", rooms);
-  console.log("Friends:", friends);
-  console.log("DMs:", dms);
-}
-
-function handleWS(data) {
-  console.log("WS:", data);
-}
-
-init();
+  loadUserPanel();
+  loadRooms();
+  loadDMs();
+  loadFriends();
+  setupWS();
+  setupSidebarToggle();
+};
