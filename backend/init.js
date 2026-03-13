dc267a79d5f37d86f6984f2724936f86c9ddd090
haruaki <haruakinahidlose@gmail.com>
2026-03-13 09:15:54 +0900

    Update init.js

--- backend/init.js
+++ backend/init.js
@@ -1,4 +1,4 @@
-import db from "./backend/db.js";
+import db from "./db.js";
 import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
@@ -6,7 +6,7 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const schemaPath = path.join(__dirname, "backend/schema.sql");
+const schemaPath = path.join(__dirname, "schema.sql");
 const schema = fs.readFileSync(schemaPath, "utf8");
 
 async function init() {
