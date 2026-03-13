3da272f579539af60ef6be131540bc38e50a3bdd
haruaki <haruakinahidlose@gmail.com>
2026-03-13 13:08:47 +0900

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -2,10 +2,10 @@ import pkg from "pg";
 const { Pool } = pkg;
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
 });
 
 export default {
-    query: (text, params) => pool.query(text, params)
+  query: (text, params) => pool.query(text, params)
 };
